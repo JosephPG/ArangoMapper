5ebd99b8811a48dd33d2fb8026b564ba928dc5c0
joseph <saiborack@gmail.com>
2026-04-18 17:17:24 -0500

    Configure dsl for field collection

--- app/collections.py
+++ app/collections.py
@@ -26,27 +26,3 @@ class Interconnection(CollectionEdge[Device, Device]):
     _graph_name: ClassVar[str] = "interconnectionsgraph"
 
     type: str
-
-
-# 1. La Estructura de Conexiones
-# Aunque haya una sola Celda, ella "alimenta" a dos caminos distintos:
-
-#     Locacion A (Interno):
-#         Interconnection 1: Celda A -> Transformador A1
-#         Interconnection 2: Celda A -> Transformador A2
-#     Locacion B (Interno):
-#         Interconnection 3: Celda B -> Transformador B1
-#         Interconnection 4: Celda B -> Transformador B2
-
-# 2. Las "Interconexiones" (Los cables de larga distancia)
-# Aquí es donde ocurre el emparejamiento que mencionas:
-
-#     Interconnection 1: Transformador A1 -cable1-> Transformador B1
-#     Interconnection 2: Transformador A2 -cable2-> Transformador B2
-
-
-# (Locacion A)Celda-Transformador1 -cable-> Transformador1-|
-#                 |-Transformador2 -cable-> Transformador2-Celda(Locacion B)
-#
-# Donde la conexion logia de dos sitios se llama Route y la conexion fisica de dos dispositvos se llama interconexion
-# se va a menajar como equipos Transformador y Celda
--- tests/test_metaclass.py
+++ tests/test_metaclass.py
@@ -5,19 +5,19 @@ from app.database.manager import CollectionManager
 
 
 def test_model_not_instanced():
-    assert Location.id == "_id"
-    assert Location.key == "_key"
-    assert Location.name == "name"
-    assert Device.id == "_id"
-    assert Device.key == "_key"
-    assert Device.name == "name"
-    assert Device.type == "type"
-    assert Route.id == "_id"
-    assert Route.key == "_key"
-    assert Route.id_from == "_from"
-    assert Route.id_to == "_to"
-    assert Route.vertex_from == "vertex_from"
-    assert Route.vertex_to == "vertex_to"
+    assert Location.id.target == "_id"
+    assert Location.key.target == "_key"
+    assert Location.name.target == "name"
+    assert Device.id.target == "_id"
+    assert Device.key.target == "_key"
+    assert Device.name.target == "name"
+    assert Device.type.target == "type"
+    assert Route.id.target == "_id"
+    assert Route.key.target == "_key"
+    assert Route.id_from.target == "_from"
+    assert Route.id_to.target == "_to"
+    assert Route.vertex_from.target == "vertex_from"
+    assert Route.vertex_to.target == "vertex_to"
 
 
 def test_model_instanced(db: StandardDatabase):
@@ -33,8 +33,8 @@ def test_model_instanced(db: StandardDatabase):
 
     route = Route(vertex_from=location, vertex_to=location)
 
-    assert location.id and location.id != "_id"
-    assert location.key and location.key != "_key"
+    assert isinstance(location.id, str)
+    assert isinstance(location.key, str)
     assert location.name == "nombre"
     assert isinstance(route.vertex_from, Location)
     assert isinstance(route.vertex_to, Location)
