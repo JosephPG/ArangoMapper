027576bcae43072ab6dabee22628257e95ac9e45
joseph <saiborack@gmail.com>
2026-04-23 00:07:21 -0500

    Add transaction

--- app/database/conn.py
+++ app/database/conn.py
@@ -1,5 +1,7 @@
+from typing import Callable
+
 from arango import ArangoClient
-from arango.database import StandardDatabase
+from arango.database import StandardDatabase, TransactionDatabase
 from loguru import logger
 
 from app.database.migrate import sync_migration
@@ -28,3 +30,32 @@ def get_db() -> StandardDatabase:
     sync_migration(_db)
 
     return _db
+
+
+def execute_transaction(
+    process: Callable, read: list[str] = [], write: list[str] = []
+) -> any:
+    """https://docs.python-arango.com/en/main/transaction.html"""
+
+    db: StandardDatabase = get_db()
+    txn: TransactionDatabase | None = None
+
+    try:
+        txn = db.begin_transaction(read=read, write=write)
+        tid = txn.transaction_id
+
+        logger.info(f"Start transaction '{tid}'")
+
+        response = process(txn)
+        txn.commit_transaction()
+
+        logger.info(f"Close transaction '{tid}'")
+
+        return response
+    except Exception as _:
+        if txn is None:
+            raise
+
+        txn.abort_transaction()
+        logger.error(f"Abort transaction '{tid}'")
+        raise
